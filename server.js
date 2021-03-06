243e237e92810a1c042945299531bd53d6943c23
Stephen Wright <wrightsah@icloud.com>
2021-03-06 13:15:50 -0500

    initialized server

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+// Create express instance
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({defaultLayout}))
+app.set('view engine', 'handlebars');
+
+
+// Start Server
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
