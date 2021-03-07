8fb008edecd8013477561f95ab7259e2baee0727
Stephen Wright <wrightsah@icloud.com>
2021-03-07 11:54:22 -0500

    created db seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-- Create 3 sample burgers
+
+INSERT INTO burgers (burger_name)
+VALUES ('Great Value Burger')
+
+INSERT INTO burgers (burger_name)
+VALUES ('Tesla Burger')
+
+INSERT INTO burgers (burger_name)
+VALUES ('Amazon Basics Burger')
