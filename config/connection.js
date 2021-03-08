ee9c753ccc29a2752eb2d1a4e8a48989d5486081
Stephen Wright <wrightsah@icloud.com>
2021-03-08 15:44:27 -0500

    created connection js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,27 @@
-// Set up Node to MySQL connection
+// Require mysql
 
+const mySQL = require('mysql');
+
+// Set up connection
+
+const connection = mySQL.createConnection({
+    host: 'localhost', 
+    port: 3306, 
+    user: 'root', 
+    password: 'Mysql2021',
+    database: 'burgers_db'
+});
+
+// Initiate MySQL connection
+
+connection.connect((err) => {
+    if (err) {
+        console.log(`error connecting: ${err.stack}`);
+        return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+});
 
 // Export module
 
+module.exports = connection; 
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Set up express app to handle data parsing
+app.subscribe(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
