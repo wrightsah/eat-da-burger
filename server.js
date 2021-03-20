0f60c4d4277805395d18416b967240156607b8cd
Stephen Wright <wrightsah@icloud.com>
2021-03-20 01:46:02 -0400

    this commit came from vscode menu

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 // Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
-const orm = require('./config/orm');
+const orm = require("./config/orm");
 
 // Create express instance
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Use static files
-app.use(express.static(('public')));
+app.use(express.static("public"));
 
 // Set up express app to handle data parsing
-app.subscribe(express.urlencoded({ extended: true}));
+app.subscribe(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes 
+// Routes
 // app.use(routes);
 
 // GET
 
-app.get('/', (req, res) => {
-    orm.selectAll('burgers', function(data) {
-        res.render('index', { burgers : data });
-    });
+app.get("/", (req, res) => {
+  orm.selectAll("burgers", function (data) {
+    res.render("index", { burgers: data });
+  });
 });
 
 // Start Server
